21e18bc740ade8b3a61d58651db424a818119826
sunny <sunnynp2@illinois.edu>
2019-04-26 16:15:42 -0500

    got the app to switch between main activity and camera screen

--- app/src/main/java/com/example/summaryapp/MainActivity.java
+++ app/src/main/java/com/example/summaryapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.summaryapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,9 +14,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button camera = findViewById(R.id.Camera);
-        camera.setOnClickListener(v -> {
-
-        }
+        final Button camera1 = findViewById(R.id.Camera);
+        camera1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent openCamera = new Intent(MainActivity.this, camera.class);
+                startActivity(openCamera);
+                // Code here executes on main thread after user presses button
+            }
+        });
     }
 }
